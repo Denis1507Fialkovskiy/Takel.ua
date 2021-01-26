eba8fde18d215ad1de9c57d5b3cae6c92d7ae893
Denis <fialim62@gmail.com>
2021-01-26 13:01:51 +0200

    26.01 Отключено запрос языка браузера

--- startup(contr).php
+++ startup(contr).php
@@ -51,7 +51,7 @@ class ControllerStartupStartup extends Controller {
 		}
 		
 		// Language Detection
-		if (!empty($this->request->server['HTTP_ACCEPT_LANGUAGE']) && !array_key_exists($code, $languages)) {
+		/*if (!empty($this->request->server['HTTP_ACCEPT_LANGUAGE']) && !array_key_exists($code, $languages)) {
 			$detect = '';
 			
 			$browser_languages = explode(',', $this->request->server['HTTP_ACCEPT_LANGUAGE']);
@@ -82,7 +82,7 @@ class ControllerStartupStartup extends Controller {
 			}
 			
 			$code = $detect ? $detect : '';
-		}
+		}*/
 		
 		if (!array_key_exists($code, $languages)) {
 			$code = $this->config->get('config_language');
